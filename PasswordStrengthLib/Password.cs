7f6c42edd7f052a39469a419f58d4b4ba5216b2d
Atilia T <atiliat@VSUs-MacBook-Air.local>
2025-08-27 16:47:45 -0400

    changed upper case function

--- PasswordStrengthLib/Password.cs
+++ PasswordStrengthLib/Password.cs
@@ -2,18 +2,22 @@
 
 public class PasswordCheck
 {
-    int count = 0; 
-    public void CheckPasswordStrength(string password)
-    { 
-        int count = 0;
+    public string CheckPasswordStrength(string password)
+    {
+        bool hasUpper = false;
+
+
         for (int counter = 0; counter < password.Length; counter++)
         {
             char current_character = password[counter];
-            if (Char.IsUpper(current_character) == true)
+
+            if (Char.IsUpper(current_character))
             {
-                counter += 1;
+                hasUpper = true;
             }
-             
+
+
         }
     }
 }
+
