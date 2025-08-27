87d1c986b54c6b814fc828964a260896d3a0557f
Atilia T <atiliat@VSUs-MacBook-Air.local>
2025-08-27 16:32:33 -0400

    created project and added upper case character check

---
+++ PasswordStrengthLib/Password.cs
@@ -0,0 +1,19 @@
+﻿namespace PasswordStrengthLib;
+
+public class PasswordCheck
+{
+    int count = 0; 
+    public void CheckPasswordStrength(string password)
+    { 
+        int count = 0;
+        for (int counter = 0; counter < password.Length; counter++)
+        {
+            char current_character = password[counter];
+            if (Char.IsUpper(current_character) == true)
+            {
+                counter += 1;
+            }
+             
+        }
+    }
+}
---
+++ PasswordStrengthTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace PasswordStrengthTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
